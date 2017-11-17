eaf65ffcea5d07b734a220ae9f6c108601890c24
Panu Luukkonen <panu.j.luukkonen@helsinki.fi>
2017-11-18 01:14:21 +0200

    some sanitation...

--- app/controllers/samples_controller.php
+++ app/controllers/samples_controller.php
@@ -12,11 +12,15 @@
     public static function store(){
       $params = $_POST;
 
+      $duration = filter_var($params['duration'], FILTER_SANITIZE_NUMBER_FLOAT,FILTER_FLAG_ALLOW_FRACTION);
+
+      if ($duration > 9999 || $duration < 0) return;
+
       $sample = new Sample(array(
         'serviceuser_id' => '1',
         'filename' => $params['filename'],
         'name' => $params['name'],
-        'duration' => $params['duration']));
+        'duration' => $duration));
 
       $sample->save();
 
