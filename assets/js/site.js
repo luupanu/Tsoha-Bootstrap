ce1045cb8c3006fa50fedb6a0da57f01bc8596f4
Panu Luukkonen <panu.j.luukkonen@helsinki.fi>
2017-11-18 01:15:44 +0200

    more js for editables

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,19 +1,30 @@
+// disable paste on editables
+$('.editable').on('paste', function(e){
+    e.preventDefault();
+  });
+// don't allow more than 50 chars on editables
+$('.editable').on('keydown', function(e){
+  if($(this).text().length >= 50 && e.keyCode != 8) {
+    e.preventDefault();
+  }
+})
+// leave focus when user presses enter
 $('.editable').keypress(function(e){
   if (e.which === 13){
     e.preventDefault();
     this.blur();
   }
 });
-
+// wip
 $('input[id=jsonUpload]').click(function(e){
   myFile = e.target.files[0];
   console.log(myFile);
 });
-
+// remove hashtags when clicked
 $('.editable').click(function (){
   this.innerText = this.innerText.replace(/#+/g, '');
 });
-
+// handle strings in editables
 $('.editable').blur(function (){
   var string = this.innerText;
 
